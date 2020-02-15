988e36f1aa6be508d8edb5b7fe0b80d099f650f5
obaya884 <taku3.884@gmail.com>
2020-02-15 16:08:28 +0900

    initial commit

---
+++ app/src/main/java/app/takumi/obayashi/todoapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package app.takumi.obayashi.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1)
+
+        listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+            // タップ時の処理
+            val adapter: ArrayAdapter<String> = listView.adapter as ArrayAdapter<String>
+            val item = adapter.getItem(position)
+            adapter.remove(item)
+            adapter.add(item)
+        }
+        
+        listView.onItemLongClickListener = AdapterView.OnItemLongClickListener { parent: AdapterView<*>?, view: View, position: Int, id: Long ->
+            // ロングタップ時の処理
+            val adapter: ArrayAdapter<String> = listView.adapter as ArrayAdapter<String>
+            val item = adapter.getItem(position)
+            adapter.remove(item)
+
+            false
+        }
+    }
+
+    fun add(view: View) {
+        val text: String = editText.text.toString()
+
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+        adapter.add(text)
+
+    }
+}
